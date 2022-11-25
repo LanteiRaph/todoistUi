3a94662a9ed673add67e673848db4c605186f94d
lanteiraph <santamulantei8@gmail.com>
2022-11-25 14:15:24 +0300

    feat: signup feature created

---
+++ assets/js/Main.js
@@ -0,0 +1,15 @@
+//
+//Window: Object
+window.addEventListener('load', () => {
+    //Get the sigin up buttom and add onclieck listener
+    const signUpBtn = document.getElementById('signUp');
+    signUpBtn.addEventListener('click', () => {
+        //Redirect to the sign page.
+        window.location = '/frontend/signup.html'
+    })
+    //Get the login button and add onclick listener
+    const loginBtn = document.getElementById('login')
+    loginBtn.addEventListener('click', () => {
+        alert('hello from login')
+    })
+});
---
+++ assets/js/signup.js
@@ -0,0 +1,34 @@
+//Sign up for the user.
+window.addEventListener('load', async () => {
+    //Get the form and add event lisneer to it
+    const form = document.querySelector('form');
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        //Get the form element
+        const form = e.target
+        //Get all the inputs of the form.
+        const inputs = form.querySelectorAll('input')
+        //Create a new user
+        const user = {}
+        //Add data to the user.
+        inputs.forEach((input) => {
+            user[input.name] = input.value
+        })
+        //Extract the data out of the form
+        await signUp(user)
+    });
+})
+const signUp =  async (user) => {
+   //Ready up the new user.
+    const response = await fetch('http://localhost:3000/signUp', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(user)
+    })
+
+    //Await the response
+    const results = await response.json()
+    console.log(results)
+}
